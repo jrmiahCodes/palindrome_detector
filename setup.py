edc2ced05e165b627260eeef661f775d63c97057
Jeremiah <jeremiahpg@gmail.com>
2024-12-12 17:50:22 -0500

    Create layout.html and a test package.

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import find_packages, setuptools
+from setuptools import find_packages, setup
 
 setup(
     name='palindrome_detector',
-    version='1.0.0'
+    version='1.0.0',
     packages=find_packages(),
 include_package_data=True,
     zip_safe=False,
